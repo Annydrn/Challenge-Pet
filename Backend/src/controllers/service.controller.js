615130b21c934dab6b5026f9b09baf71954e52aa
Annydrn <andrea.duran.cabana@gmail.com>
2023-08-05 18:42:09 -0400

    creacion de crud para los servicios de la app

--- Backend/src/controllers/service.controller.js
+++ Backend/src/controllers/service.controller.js
@@ -1,33 +1,38 @@
-import Service from "../models/service.model.js"
+import Service from "../models/service.model.js";
+import serviceSchema from "../models/service.model.js";
 
-export const getServices= async (req,res)=>{
-    const services= await serviceSchema.find()
-    res.json(services)
+export const getServices = async (req, res) => {
+  const services = await serviceSchema.find({
+    user: req.user.id, //aca le pido que me traiga las tareas solo del id(usuario) que esta autentificado
+  }).populate("user")//populate me cargara toda la informacion de ese usuario
+  res.json(services);
 };
-export const createService= async (req,res)=>{
-    const{servicio, mascota, fecha} = req.body
-    const newService= new Service({
-        servicio,
-        mascota,
-        fecha
-    });
-    const savedService= await newService.save();
-    res.json(savedService)
-}
-export const getService= async (req,res)=>{
-    await serviceSchema.findById(req.params.id)
-    if(!service) return res.status(404).json({message:" Service not found"})
-    res.json(service)
-}
-export const updateService= async (req,res)=>{
-    await serviceSchema.findByIdAndDelete(req.params.id, req.body, {
-        new:true
-    })
-    if(!service) return res.status(404).json({message:"Service not found"})
-    res.json(service)
-}
-export const deleteService= async (req,res)=>{
-    await serviceSchema.findByIdAndUpdate(req.params.id)
-    if(!service) return res.status(404).json({message:"Service not found"})
-    res.json(service)
-}
+export const createService = async (req, res) => {
+  const { servicio, mascota, fecha } = req.body;
+  console.log(req.user); //clg para verificar en terminal el id del usuario
+  const newService = new Service({
+    servicio,
+    mascota,
+    fecha,
+    user: req.user.id,
+  });
+  const savedService = await newService.save();
+  res.json(savedService);
+};
+export const getService = async (req, res) => {
+    const service= await serviceSchema.findById(req.params.id).populate("user");
+  if (!service) return res.status(404).json({ message: " Service not found" });
+  res.json(service);
+};
+export const updateService = async (req, res) => {
+  const service= await serviceSchema.findByIdAndUpdate(req.params.id, req.body,{
+    new: true,
+  });
+  if (!service) return res.status(404).json({ message: "Service not found" });
+  res.json(service);
+};
+export const deleteService = async (req, res) => {
+  const service= await serviceSchema.findByIdAndDelete(req.params.id);
+  if (!service) return res.status(404).json({ message: "Service not found" });
+  return res.sendStatus(204);
+};
--- Backend/src/routes/service.routes.js
+++ Backend/src/routes/service.routes.js
@@ -6,8 +6,15 @@ const router= Router()
 
 router.get("/services", authRequired ,getServices);//pedir todos
 router.get("/service/:id", authRequired, getService );//pedir uno
-router.post("/newservice", authRequired, createService );//publicar
-router.delete("/deleteservice/:id", authRequired , deleteService);//eliminar
-router.put("/updateservice/:id", authRequired , updateService);//editar
+router.post("/service", authRequired, createService );//publicar
+router.delete("/service/:id", authRequired , deleteService);//eliminar
+router.put("/service/:id", authRequired , updateService);//editar
+
+
+// router.get("/services", authRequired ,getServices);//pedir todos
+// router.get("/service/:id", authRequired, getService );//pedir uno
+// router.post("/newservice", authRequired, createService );//publicar
+// router.delete("/deleteservice/:id", authRequired , deleteService);//eliminar
+// router.put("/updateservice/:id", authRequired , updateService);//editar
 
 export default router
