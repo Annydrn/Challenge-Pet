3aad538710d62a9797495a2986a8a8f46363eb2a
Annydrn <andrea.duran.cabana@gmail.com>
2023-08-06 00:27:35 -0400

    actualizacion img

--- Front-end/src/api/auth.js
+++ Front-end/src/api/auth.js
@@ -2,4 +2,4 @@ import  axios  from "axios";
 
 const API = "http://localhost:3300/api/";
 
-export const registerRequest = user => axios.post(`${API}/signup`, user);
+export const registerRequest = user => axios.post(`${API}signup`, user);
