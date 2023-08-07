db9223b768bc81e83dcf6df420fe8d322632bd26
Annydrn <andrea.duran.cabana@gmail.com>
2023-08-07 16:36:43 -0400

    actualizacion.4

--- Front-end/src/components/Navbar.jsx
+++ Front-end/src/components/Navbar.jsx
@@ -7,13 +7,13 @@ function NavbarPage() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">App Pets</Navbar.Brand>
+        <Navbar.Brand href="#home" className='text-warning'>App Pets</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/signup">Registrate</Nav.Link>
-            <Nav.Link href="/login">Inicia Sesión</Nav.Link>
+            <Nav.Link href="signup">Registrate</Nav.Link>
+            <Nav.Link href="login">Inicia Sesión</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
