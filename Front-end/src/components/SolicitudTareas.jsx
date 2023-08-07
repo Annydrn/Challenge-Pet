c3937be71d99c38df69116d86c4d1d0749dcb57b
Annydrn <andrea.duran.cabana@gmail.com>
2023-08-07 10:32:58 -0400

    maqutacion ruta servicios

--- Front-end/src/components/SolicitudTareas.jsx
+++ Front-end/src/components/SolicitudTareas.jsx
@@ -1,8 +1,32 @@
+import { useForm } from "react-hook-form";
 
 const SolicitudTareas = () => {
-  return (
-    <div>SolicitudTareas</div>
-  )
-}
+  const { register, handleSubmit } = useForm();
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
 
-export default SolicitudTareas
+  return (
+    <div className="container-fluid bg-secondary py-4">
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Servicio"
+          {...register("servicio")}
+          className="w-100 bg-warning  p-2 rounded"
+          autoFocus
+        />
+        <textarea
+          rows="3"
+          placeholder="Detalles que nos quieras informar"
+          {...register("detalles")}
+          className="w-100 bg-light p-2 rounded"
+
+        ></textarea>
+        <button>Guardar</button>
+      </form>
+    </div>
+  );
+};
+
+export default SolicitudTareas;
